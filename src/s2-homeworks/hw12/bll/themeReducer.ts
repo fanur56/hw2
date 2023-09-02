721e1bc8b4e29eb3b850af42a0c22a1ef3d74ca9
fanur56 <ilasovf@gmail.com>
2023-09-02 17:24:05 +0300

    refactor HW12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,19 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ChangeThemeIdAT): {themeId: number} => {
     switch (action.type) {
-        // дописать
+        case 'SET_THEME_ID':
+            return {themeId: action.id}
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ChangeThemeIdAT = {
+    type: string
+    id: number
+}
+
+export const changeThemeId = (id: number): ChangeThemeIdAT => ({ type: 'SET_THEME_ID', id })
